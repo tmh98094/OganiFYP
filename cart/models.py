7fa294808060a2f331d91e50d66a1637cda23ff5
tmh98094 <tmh0249@gmail.com>
2020-07-17 16:24:46 +0800

    Product Listing Completed

--- cart/models.py
+++ cart/models.py
@@ -1,6 +1,8 @@
+  
 from django.contrib.auth import get_user_model
 from django.db import models
-from django_countries.fields import CountryField
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
 User = get_user_model()
 
@@ -33,12 +35,14 @@ class Shop(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=150)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='product_images')
     description = models.TextField()
-    quantity = models.IntegerField()
+    stock_quantity = models.IntegerField()
+    price = models.FloatField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    expired = models.DateTimeField()
+    expired = models.DateField()
     active = models.BooleanField(default=False)
 
     def __str__(self):
@@ -91,3 +95,11 @@ class Payment(models.Model):
     @property
     def reference_number(self):
         return f"PAYMENT-{self.order}-{self.pk}"
+    
+    
+def pre_save_product_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.title)
+
+
+pre_save.connect(pre_save_product_receiver, sender=Product)
