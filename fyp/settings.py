d72edb1ff24a037a6b354901907eeed625d47178
tmh98094 <tmh0249@gmail.com>
2020-08-18 00:43:19 +0800

    Commit

--- fyp/settings.py
+++ fyp/settings.py
@@ -174,11 +174,7 @@ if DEBUG is False:
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'surfood',
             'USER': 'postgres',
-<<<<<<< HEAD
             'PASSWORD': 'qwerasdf',
-=======
-            'PASSWORD': '######',
->>>>>>> 95497d24d4fcf6fb9ff54583e125c6075369615e
             'HOST': '127.0.0.1',
             'PORT': '5432',
         }
